60332bcf1076c6678b07e7e7c2b6df882538c9eb
NAULI-KE <58718670+NAULI-KE@users.noreply.github.com>
2020-01-03 19:05:17 +0300

    update home controller
    update home controller

--- src/main/java/com/MBASE/controller/Home.java
+++ src/main/java/com/MBASE/controller/Home.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Home {
 	
-	@RequestMapping("/")
+	@RequestMapping("/welcome")
 	public String welcome() {
 		return "Jenkins pipeline config script";
 	}
